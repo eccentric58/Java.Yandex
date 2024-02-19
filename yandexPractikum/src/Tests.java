beb72e49adec88703dca067d179f6e4766275207
eccentric <zhosan00@list.ru>
2024-02-19 20:45:21 +0300

    Добавил нерабочий тест

---
+++ yandexPractikum/src/Tests.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    @Test
+    public void test1() {
+        Shopping shopping = new Shopping();
+        List<String> listShoppingTest = new ArrayList<>();
+        listShoppingTest.add("Пельмени");
+        listShoppingTest.add("Вареники");
+        listShoppingTest.add("Картошка");
+
+    }
+
+}
