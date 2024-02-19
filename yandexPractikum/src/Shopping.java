95ca5cda422ea05967a3a7a5532bd28e823b838c
eccentric <zhosan00@list.ru>
2024-02-19 20:07:27 +0300

    Добавил реализацию выбора комманд, список

--- yandexPractikum/src/Shopping.java
+++ yandexPractikum/src/Shopping.java
@@ -1,7 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Shopping {
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class Shopping {
     public static void printMenu() {
         System.out.println("Выберите одну из команд:");
         System.out.println("1. Добавить товар в список");
@@ -10,10 +11,37 @@ public class Shopping {
         System.out.println("4. Завершить работу");
     }
 
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        printMenu();
+        List<String> listShopping = new ArrayList<>();
 
+        System.out.println("Вас приветствует список покупок!");
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("Введите товар, который хотите добавить:");
+                String productName = scanner.next();
+                listShopping.add(productName);
+            } else if (command == 2) {
+                System.out.println("Список товаров: ");
+                int count = 1;
+                for (String i : listShopping) {
+                    System.out.print(count + ". " + i);
+                    System.out.println();
+                    count++;
+                }
+            } else if (command == 3) {
+                listShopping.clear();
+                System.out.println("Список товаров очистен.");
+            } else if (command == 4) {
+                System.out.println("Работа приложения списка покупок завершена.");
+                break;
+            }
+            System.out.println();
+        }
     }
 }
