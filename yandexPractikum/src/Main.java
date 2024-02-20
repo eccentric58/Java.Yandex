fec125ea354e28c46e7f681c1aba5a17ead24b89
eccentric <zhosan00@list.ru>
2024-02-21 01:41:29 +0300

    Добавил ссылку на гитхаб

--- yandexPractikum/src/Main.java
+++ yandexPractikum/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+// Проект выложен на открытый github: https://github.com/eccentric58/Java.Yandex
 public class Main {
     public static void main(String[] args) {
 
