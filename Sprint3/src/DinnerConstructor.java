8031eb945ebbecd52965f77f4a45f928c959effc
eccentric <zhosan00@list.ru>
2024-03-19 10:55:46 +0300

    внес правки

--- Sprint3/src/DinnerConstructor.java
+++ Sprint3/src/DinnerConstructor.java
@@ -1,10 +1,40 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DinnerConstructor {
-
+    Random random = new Random();
+    Scanner scanner;
     HashMap<String, ArrayList<String>> dinner = new HashMap<>();
+
+    public DinnerConstructor(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    void addNewDish() {
+        System.out.println("Введите тип блюда:");
+        String dishType = scanner.nextLine();
+        System.out.println("Введите название блюда:");
+        String dishName;
+        ArrayList<String> dishes;
+        if (dinner.containsKey(dishType)) {
+            dishes = dinner.get(dishType);
+        } else {
+            dishes = new ArrayList<>();
+        }
+        do {
+            dishName = scanner.nextLine();
+            if (!dishes.contains(dishName)) {
+                dishes.add(dishName);
+            } else {
+                System.out.println("Блюдо уже есть!");
+            }
+        } while (!dishName.equals(""));
+
+        dishes.remove(dishes.size() - 1);
+        dinner.put(dishType, dishes);
+    }
 
     void printDishes() {
         for (String s : dinner.keySet()) {
@@ -13,5 +43,39 @@ public class DinnerConstructor {
             System.out.println(list);
         }
         System.out.println();
+    }
+
+    // Перенес методы, но по ТЗ было написано оставить эти 2 метода в классе Main вроде как)
+    void generateDishCombo() {
+        System.out.println("Начинаем конструировать обед...");
+
+        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
+        int numberOfCombos = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Вводите типы блюда, разделяя символом переноса строки (enter)." +
+                " Для завершения ввода введите пустую строку");
+        String nextItem = scanner.nextLine();
+        ArrayList<String> comboDishes = new ArrayList<>();
+        comboDishes.add(nextItem);
+
+        while (!nextItem.isEmpty()) {
+            nextItem = scanner.nextLine();
+            comboDishes.add(nextItem);
+        }
+
+        comboDishes.remove(comboDishes.size() - 1);
+        // тут есть ошибка или "фича", если ввести после количество наборов пустую строчку, то метод вернет 3 пустых комбо))
+        for (int i = 0; i < numberOfCombos; i++) {
+            System.out.println("Комбо " + (i + 1));
+            ArrayList<String> comboFinish = new ArrayList<>();
+            for (String s : comboDishes) {
+                if (dinner.containsKey(s)) {
+                    ArrayList<String> d = dinner.get(s);
+                    comboFinish.add(d.get(random.nextInt(d.size())));
+                }
+            }
+            System.out.println(comboFinish);
+        }
     }
 }
--- Sprint3/src/Main.java
+++ Sprint3/src/Main.java
@@ -1,28 +1,24 @@
-import java.util.Random;
 import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Main {
-    static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
     static DinnerConstructor dc;
 
     public static void main(String[] args) {
-        dc = new DinnerConstructor();
+        dc = new DinnerConstructor(scanner);
 
         while (true) {
             printMenu();
-            String command = scanner.nextLine();
+            String command = scanner.nextLine().trim();
 
             switch (command) {
                 case "1":
-                    addNewDish();
+                    dc.addNewDish();
                     break;
                 case "2":
                     dc.printDishes();
                     break;
                 case "3":
-                    generateDishCombo();
+                    dc.generateDishCombo();
                     break;
                 case "4":
                     return;
@@ -36,62 +32,5 @@ public class Main {
         System.out.println("2 - Вывести список блюд");
         System.out.println("3 - Сгенерировать комбинации блюд");
         System.out.println("4 - Выход");
-    }
-
-    private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
-        String dishType = scanner.nextLine();
-        System.out.println("Введите название блюда:");
-        String dishName;
-        ArrayList<String> dishes;
-        if (dc.dinner.containsKey(dishType)) {
-            dishes = dc.dinner.get(dishType);
-        } else {
-            dishes = new ArrayList();
-        }
-        do {
-            dishName = scanner.nextLine();
-            if (!dishes.contains(dishName)) {
-                dishes.add(dishName);
-            } else {
-                System.out.println("Блюдо уже есть!");
-            }
-        } while (!dishName.equals(""));
-
-        dishes.remove(dishes.size() - 1);
-        dc.dinner.put(dishType, dishes);
-    }
-
-    private static void generateDishCombo() {
-        System.out.println("Начинаем конструировать обед...");
-
-        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
-        int numberOfCombos = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
-        String nextItem = scanner.nextLine();
-        ArrayList<String> comboDishes = new ArrayList<>();
-        comboDishes.add(nextItem);
-
-        while (!nextItem.isEmpty()) {
-            nextItem = scanner.nextLine();
-            comboDishes.add(nextItem);
-        }
-
-        comboDishes.remove(comboDishes.size() - 1);
-
-        for (int i = 0; i < numberOfCombos; i++) {
-            System.out.println("Комбо " + (i + 1));
-            ArrayList<String> comboFinish = new ArrayList<>();
-            for (String s : comboDishes) {
-                if (dc.dinner.containsKey(s)) {
-                    ArrayList<String> d = dc.dinner.get(s);
-                    comboFinish.add(d.get(random.nextInt(d.size())));
-                }
-            }
-            System.out.println(comboFinish);
-
-        }
     }
 }
